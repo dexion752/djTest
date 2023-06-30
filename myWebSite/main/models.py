fdf2046bf0e524c406e04631a126837a97f77090
dexion752 <dexion.kong@gmail.com>
2023-06-30 16:25:05 +0900

    model, view updated

--- myWebSite/main/models.py
+++ myWebSite/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Testdata(models.Model):
+    id = models.IntegerField(primary_key=True)
+    entry = models.TextField(blank=True, null=True)
+    hanja = models.TextField(db_column='hanJa', blank=True, null=True)  # Field name made lowercase.
+    metaterm = models.TextField(db_column='metaTerm', blank=True, null=True)  # Field name made lowercase.
+    field = models.TextField(blank=True, null=True)
+    pageno = models.IntegerField(db_column='pageNo', blank=True, null=True)  # Field name made lowercase.
+    simpleex = models.TextField(db_column='simpleEx', blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'testdata'
--- myWebSite/main/views.py
+++ myWebSite/main/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+#  def index(request):
+#      return render(request, "main/index.html")
+
+from .models import Testdata
+
+#  def testdata_view(request):
+#      data = Testdata.objects.all()
+#      # print(data)
+#      return render(request, 'main/index.html', {"data": data})
+
 def index(request):
-    return render(request, "main/index.html")
+    data = Testdata.objects.all()
+    # print(data)
+    return render(request, 'main/index.html', {"data": data})
